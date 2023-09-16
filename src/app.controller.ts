da5dc29dd19345111ab8aa31ff5dd8d83a7120f2
Code-Nature <codenature01001@gmail.com>
2023-09-16 17:50:03 -0500

    sending parameters examples

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,26 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello Platzi';
+  }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'yo soy nuevo';
+  }
+  // En nextJS para las rutas no es necesario incluir el slash(/)
+  @Get('/ruta/')
+  newEndpoint2() {
+    return 'yo soy nuevo con /slash/';
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
+    return `product ${productId} and ${id}`;
   }
 }
